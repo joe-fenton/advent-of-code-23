a2f229bb1e0d83b7730b80c256baf37f1d09723f
Joe Fenton <joe.fenton@coop.co.uk>
2023-12-04 21:14:33 +0000

    Day1 - Tests passing for c#

---
+++ csharp/src/day1/Day1.cs
@@ -0,0 +1,9 @@
+﻿namespace adventofcode2023.day1;
+
+public static class Day1
+{
+  public static bool CheckTest()
+  {
+    return true;
+  }
+}
---
+++ csharp/test/day1Test/Day1.cs
@@ -0,0 +1,12 @@
+using adventofcode2023.day1;
+
+namespace adventofcode2023Tests;
+public class Day1Test
+{
+    [Fact]
+    public void Day1_shouldReturnTrue()
+    {
+
+        Assert.True(Day1.CheckTest());
+    }
+}
