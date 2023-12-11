3ed59c0c179a5ea5de13a462212c5a1f71e20eea
Joe Fenton <joe.fenton@coop.co.uk>
2023-12-11 00:25:05 +0000

    First pass at adding coloured cubes

--- csharp/src/day2/Day2.cs
+++ csharp/src/day2/Day2.cs
@@ -1,6 +1,88 @@
-﻿namespace day2;
+﻿
+
+using System.Text.RegularExpressions;
+
+namespace day2;
 
 public class Class1
 {
 
+}
+
+public class GameReader
+{
+  private IEnumerable<Game> _games;
+
+  public GameReader(params string[] games)
+  {
+    _games = games.Select(game => new Game(game));
+  }
+
+  public Game GetGameById(int id)
+  {
+      return _games.Single(game => game.ID == id);
+  }
+
+  
+}
+
+public class Game
+{
+  private Dictionary<Colours, List<int>> _cubes;
+  public enum Colours
+  {
+    Red,
+    Green,
+    Blue
+  }
+
+  public Game(string gameString)
+  {
+      _cubes = new Dictionary<Colours, List<int>>();
+      extractGameDefinition(gameString);
+  }
+
+  public int ID
+  {
+    get;
+    private set;
+  }
+
+  public int ReadCubeByColour(string colour)
+  {
+    return _cubes[GetColours(colour)][0];
+  }
+
+  private void extractGameDefinition(string game)
+  {
+    var separators = new string[] {":", ","};
+    var splitGameString = game.Split(separators, StringSplitOptions.TrimEntries);
+    ID = int.Parse(Regex.Match(splitGameString[0], @"\d+").Value);
+    Regex myRegex = new Regex(@"\b(Red|Blue|Green)");
+    for (int i = 1; i < splitGameString.Length - 1; i++)
+    {
+      
+      var results = myRegex.Split(splitGameString[i]);
+      Colours cubeColour = GetColours(results[1]);
+      if(_cubes.ContainsKey(cubeColour))
+      {
+        _cubes[cubeColour].Add(int.Parse(results[0]));
+      } 
+      else
+      {
+        _cubes.Add(cubeColour, new List<int>{int.Parse(results[0])});
+      }
+    }
+  }
+
+  private static Colours GetColours(string colour)
+  {
+    switch (colour)
+    {
+      case "Red": return Colours.Red;
+      case "Blue": return Colours.Blue;
+      case "Green": return Colours.Green;
+      default: return Colours.Blue;
+    }
+  }
 }
--- csharp/test/day2Test/Day2Test.cs
+++ csharp/test/day2Test/Day2Test.cs
@@ -1,10 +1,30 @@
+using day2;
+
 namespace day2Test;
 
 public class Day2Test
 {
     [Fact]
-    public void Test1()
+    public void shouldReadEmptyGamesWithNoCubes()
     {
+        var gameReader = new GameReader("Game 1:", "Game 2:");
+        var game1 = gameReader.GetGameById(1);
+        var game2 = gameReader.GetGameById(2);
 
+        Assert.NotNull(gameReader.GetGameById(1));
+        Assert.NotNull(gameReader.GetGameById(2));
+        Assert.Equal(1, game1.ID);
+        Assert.Equal(2, game2.ID);
+    }
+
+    [Fact]
+    public void shouldReadGamesWithRedColours()
+    {
+      var gameReader = new GameReader("Game 1: 4 Red, 3 Blue", "Game 2: 6 Red, 2 Yellow");
+      var game1 = gameReader.GetGameById(1);
+      var game2 = gameReader.GetGameById(2);
+
+      Assert.Equal(4, game1.ReadCubeByColour("Red"));
+      Assert.Equal(6, game2.ReadCubeByColour("Red"));
     }
 }
